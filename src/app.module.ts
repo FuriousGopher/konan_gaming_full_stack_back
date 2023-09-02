cbc6467f5fd1f960b97bdf0ed10c3415015f0744
ivankabar <alkazavr94@mail.com>
2023-09-02 21:17:08 +0200

    registration and connect to web db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { DatabaseModule } from './db/database.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    AuthModule,
+    DatabaseModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,13 @@
+import { Controller, Post, Body } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { RegistrationDto } from './dto/create-auth.dto';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('registration')
+  async registration(@Body() createAuthDto: RegistrationDto) {
+    return this.authService.create(createAuthDto);
+  }
+}
