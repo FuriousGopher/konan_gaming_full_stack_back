fc61b94b33221398dd93442b8b380622b130e251
ivankabar <alkazavr94@mail.com>
2023-09-05 01:08:57 +0200

    fixing game-data.json 1

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { useContainer } from '@nestjs/class-validator';
-import * as cookieParser from 'cookie-parser';
+import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
