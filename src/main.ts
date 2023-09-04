55910f35d9ded2e339ce3dae3baac5443dd7afea
ivankabar <alkazavr94@mail.com>
2023-09-04 18:43:54 +0200

    added vercel.json for deploy

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { useContainer } from '@nestjs/class-validator';
+import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  useContainer(app.select(AppModule), { fallbackOnErrors: true });
+  app.enableCors();
+  app.use(cookieParser());
+
   await app.listen(3000);
 }
 bootstrap();
